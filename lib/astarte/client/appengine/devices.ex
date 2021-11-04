9e58d5f683b818607d75f5bc8f1de3003ec1b43f
Sergey Zakhlypa <sergey.zakhlypa@secomind.com>
2021-11-04 09:41:21 +0100

    Add get_datastream_data to AppEngine.Devices
    Signed-off-by: Sergey Zakhlypa <sergey.zakhlypa@secomind.com>

--- lib/astarte/client/appengine/devices.ex
+++ lib/astarte/client/appengine/devices.ex
@@ -45,4 +45,18 @@ defmodule Astarte.Client.AppEngine.Devices do
       end
     end
   end
+
+  def get_datastream_data(%AppEngine{} = client, device_id, interface, query_opts \\ [])
+      when is_binary(device_id) and is_binary(interface) do
+    tesla_client = client.http_client
+    request_path = "devices/#{device_id}/interfaces/#{interface}"
+
+    with {:ok, %Tesla.Env{} = result} <- Tesla.get(tesla_client, request_path, query: query_opts) do
+      if result.status == 200 do
+        {:ok, result.body}
+      else
+        {:error, %APIError{status: result.status, response: result.body}}
+      end
+    end
+  end
 end
