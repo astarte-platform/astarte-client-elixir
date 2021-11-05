c0bc844097bde672f7aaaecd66839eaff61dd90c
Sergey Zakhlypa <sergey.zakhlypa@secomind.com>
2021-11-05 15:00:39 +0100

    Add Astarte.Client.AppEngine.Devices endpoints
    list
    get_device_interfaces
    
    Signed-off-by: Sergey Zakhlypa <sergey.zakhlypa@secomind.com>

---
+++ lib/astarte/client/appengine/pagination.ex
@@ -0,0 +1,80 @@
+#
+# This file is part of Astarte.
+#
+# Copyright 2022 SECO Mind
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+defmodule Astarte.Client.AppEngine.Pagination do
+  @moduledoc false
+  alias Astarte.Client.{APIError, AppEngine}
+
+  def list(%AppEngine{} = client, request_path, opts \\ []) when is_binary(request_path) do
+    tesla_client = client.http_client
+    query = Keyword.get(opts, :query, [])
+
+    list_data =
+      Stream.unfold({tesla_client, request_path, query}, &list_stream/1)
+      |> Enum.to_list()
+      |> List.flatten()
+
+    case List.last(list_data) do
+      {:error, _} = error -> error
+      _ -> {:ok, %{"data" => list_data}}
+    end
+  end
+
+  defp list_stream(nil), do: nil
+
+  defp list_stream({tesla_client, request_path, query} = list_params) do
+    case Tesla.get(tesla_client, request_path, query: query) do
+      {:ok, %Tesla.Env{status: 200, body: body}} ->
+        process_list_response(body, list_params)
+
+      {:ok, %Tesla.Env{status: status, body: body}} ->
+        {{:error, %APIError{status: status, response: body}}, nil}
+
+      error ->
+        {error, nil}
+    end
+  end
+
+  defp process_list_response(%{"data" => entity_list} = body, list_params) do
+    {entity_list, next_list_params(body, list_params)}
+  end
+
+  defp next_list_params(body, {tesla_client, request_path, query}) do
+    with {:ok, links} <- links(body),
+         {:ok, link} <- next_link(links),
+         token when not is_nil(token) <- next_token(link) do
+      {tesla_client, request_path, Keyword.put(query, :from_token, token)}
+    else
+      _ -> nil
+    end
+  end
+
+  defp links(%{"links" => links}) when is_map(links), do: {:ok, links}
+  defp links(_), do: {:error, :no_links_data}
+
+  defp next_link(%{"next" => next_link}) when is_binary(next_link), do: {:ok, next_link}
+  defp next_link(_), do: {:error, :no_next_link}
+
+  defp next_token(link) when is_binary(link) do
+    link
+    |> URI.parse()
+    |> Map.get(:query)
+    |> URI.decode_query()
+    |> Map.get("from_token")
+  end
+end
