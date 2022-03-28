50a2fd4f9e31a34a7c6f92085cd6f6e43c43069d
Sergey Zakhlypa <sergey.zakhlypa@secomind.com>
2022-03-28 12:11:04 +0200

    Add excoveralls
    Signed-off-by: Sergey Zakhlypa <sergey.zakhlypa@secomind.com>

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule Astarte.Client.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -27,7 +34,8 @@ defmodule Astarte.Client.MixProject do
       {:jason, "~> 1.0"},
       {:joken, "~> 2.4"},
       {:x509, "~> 0.8"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 end
