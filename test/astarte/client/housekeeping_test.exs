b6e6c35ea03030f55144ad832c03fd9f82c7de0d
Sergey Zakhlypa <sergey.zakhlypa@secomind.com>
2022-10-03 11:44:41 +0200

    Add docs for Astarte.Client.Housekeeping module
    Signed-off-by: Sergey Zakhlypa <sergey.zakhlypa@secomind.com>

--- test/astarte/client/housekeeping_test.exs
+++ test/astarte/client/housekeeping_test.exs
@@ -1,7 +1,7 @@
 #
 # This file is part of Astarte.
 #
-# Copyright 2021 SECO Mind
+# Copyright 2021-2022 SECO Mind
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,7 +18,6 @@
 
 defmodule Astarte.Client.HousekeepingTest do
   use ExUnit.Case
-  doctest Astarte.Client.Housekeeping
 
   alias Astarte.Client.Housekeeping
   alias Astarte.Client.Housekeeping.Realms
